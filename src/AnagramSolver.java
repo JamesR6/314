58b4ccda08b49fdec84045ff09e4c79a544892a7
JamesR6 <86026411+JamesR6@users.noreply.github.com>
2024-10-20 10:19:15 -0500

    6 files ready

--- src/AnagramSolver.java
+++ src/AnagramSolver.java
@@ -1,3 +1,40 @@
+/* CS 314 STUDENTS: FILL IN THIS HEADER.
+ *
+ * Student information for assignment:
+ *
+ *  On my honor, <NAME>, this programming assignment is my own work
+ *  and I have not provided this code to any other student.
+ *
+ *  UTEID:
+ *  email address:
+ *  TA name:
+ *  Number of slip days I am using:
+ */
+
+
+import java.util.List;
+import java.util.Set;
+
 public class AnagramSolver {
-    
+
+    /*
+     * pre: list != null
+     * @param list Contains the words to form anagrams from.
+     */
+    public AnagramSolver(Set<String> dictionary) {
+        // CS314 Students, add your code here
+    }
+
+    /*
+     * pre: maxWords >= 0, s != null, s contains at least one 
+     * English letter.
+     * Return a list of anagrams that can be formed from s with
+     * no more than maxWords, unless maxWords is 0 in which case
+     * there is no limit on the number of words in the anagram
+     */
+    public List<List<String>> getAnagrams(String s, int maxWords) {
+        // CS314 Students, add your code here.
+        return null;
+    }
+
 }
--- src/LetterInventory.java
+++ src/LetterInventory.java
@@ -1,3 +1,4 @@
 public class LetterInventory {
+    //self made class
     
 }

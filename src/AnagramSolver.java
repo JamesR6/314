ca3b092e5f513e5d3064f4a0ce47b74b51168106
JamesR6 <jpascualsr06@gmail.com>
2024-10-23 23:53:36 -0500

    ajdg

--- src/AnagramSolver.java
+++ src/AnagramSolver.java
@@ -12,18 +12,27 @@
  */
 
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class AnagramSolver {
-    
 
+    //instance variable
+    Map<String, LetterInventory> dictionary;
+    
     /*
      * pre: list != null
      * @param list Contains the words to form anagrams from.
      */
     public AnagramSolver(Set<String> dictionary) {
         // CS314 Students, add your code here
+        this.dictionary = new TreeMap<String, LetterInventory>();
+        for (String word : dictionary) {
+            this.dictionary.put(word, new LetterInventory(word));
+        }
     }
 
     /*
@@ -34,8 +43,38 @@ public class AnagramSolver {
      * there is no limit on the number of words in the anagram
      */
     public List<List<String>> getAnagrams(String s, int maxWords) {
-        // CS314 Students, add your code here.
-        return null;
+
+        LetterInventory target = new LetterInventory(s);
+        if (maxWords < 0 || s == null || s.length() == 0 || target.isEmpty()) {
+            throw new IllegalArgumentException("invalid arguements");
+        }
+
+        //TODO choice of map
+        Map<String, LetterInventory> virtualDict = new TreeMap<String,LetterInventory>();
+        for (String word : dictionary.keySet()) {
+            if (target.substract(dictionary.get(word)) != null) {
+                virtualDict.put(word, new LetterInventory(word));
+            }
+        }
+
+        if (maxWords == 0 || maxWords > s.length()) {
+            maxWords = s.length();
+        }
+
+        return recurAnagrams(target, maxWords, virtualDict, 0, new ArrayList<String>());
     }
+
+    private List<List<String>> recurAnagrams(LetterInventory lettersLeft, int maxWords, 
+    Map<String, LetterInventory> virtualDict, int index, ArrayList<String> curr) {
+        //base cases
+        if (whatever) {
+            do whatever
+        }
+
+        List<String> result = new ArrayList<>();
+        //recursive step
+        return result;
+    }
+    //newest change
 
 }
